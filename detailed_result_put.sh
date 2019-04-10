945bd834027d65a61e7f30c391efe8cc53af64e7
Tthomas63 <mr.tyler.thomas@gmail.com>
2019-04-10 11:53:32 -0700

    Check entr status code in loop and break if code is greater than 0

--- detailed_result_put.sh
+++ detailed_result_put.sh
@@ -4,4 +4,5 @@
 while sleep 1 ; do
     # -d makes entr watch for non-existing files as well
     echo $2 | entr -d python /worker/detailed_result_put.py $1 $2
+    if [ ${PIPESTATUS[1]} -gt 0 ]; then break; fi
 done
